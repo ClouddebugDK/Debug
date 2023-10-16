afdea5ea207dae26bdbf1b03e715e087cc4b61d2
ClouddebugDK <147859957+ClouddebugDK@users.noreply.github.com>
2023-10-16 16:57:53 +0200

    IntuneWin32App

---
+++ Scripts/Install Modules/Install-IntuneWin32App.ps1
@@ -0,0 +1,24 @@
+# Install the required NuGet provider if not already installed
+if (-not (Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction SilentlyContinue)) {
+    Install-PackageProvider -Name NuGet -Force
+}
+
+# Define required modules
+$requiredModules = @("IntuneWin32App")
+
+foreach ($module in $requiredModules) {
+    if (Get-Module -Name $module -ListAvailable) {
+        $latestVersion = (Find-Module -Name $module).Version
+        $installedVersion = (Get-Module -Name $module).Version
+
+        if ($latestVersion -gt $installedVersion) {
+            Write-Host "Updating $module module to version $latestVersion."
+            Update-Module -Name $module -Force
+        } else {
+            Write-Host "Latest version of $module module is already installed."
+        }
+    } else {
+        Install-Module -Name $module -Force -Scope CurrentUser -AcceptLicense
+        Write-Host "$module module has been installed."
+    }
+}
